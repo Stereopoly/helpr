a88199f761f5329ef8206e918274f7977e2cd3ce
Oscar Bjorkman <objorkman@live.com>
2015-07-22 16:47:49 -0700

    Add Chat page

--- SimpleApp/ChatViewController.swift
+++ SimpleApp/ChatViewController.swift
@@ -8,12 +8,45 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITextFieldDelegate {
+    
+    var kbHeight: CGFloat!
+    
+    @IBOutlet weak var chatField: UITextField!
 
+    @IBAction func sendButton(sender: AnyObject) {
+        
+    }
+    @IBOutlet weak var sendButtonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        chatField.delegate = self
+        
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        while chatField.text.isEmpty {
+            chatField.enabled = false
+        }
+        chatField.enabled = true
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+        
+        sendButtonOutlet.enabled = false
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +54,36 @@ class ChatViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            if let keyboardSize =  (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+                kbHeight = keyboardSize.height
+                self.animateTextField(true)
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.animateTextField(false)
+    }
+    
+    func animateTextField(up: Bool) {
+        var movement = (up ? (-kbHeight + 45) : (kbHeight - 45))
+        
+        UIView.animateWithDuration(0.3, animations: {
+            self.view.frame = CGRectOffset(self.view.frame, 0, movement)
+        })
+    }
 
     /*
     // MARK: - Navigation
