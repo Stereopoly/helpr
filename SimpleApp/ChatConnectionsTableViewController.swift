eabd8df6d79159b702a91ee49121a60f700270fb
Oscar Bjorkman <objorkman@live.com>
2015-08-04 13:12:03 -0700

    Add receiver parse column

---
+++ SimpleApp/ChatConnectionsTableViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ChatConnectionsTableViewController.swift
+//  Wanna Help
+//
+//  Created by Oscar Bjorkman on 8/4/15.
+//  Copyright (c) 2015 Oscar Bjorkman. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+import SwiftSpinner
+
+var selectedChat = ""
+
+class ChatConnectionsTableViewController: PFQueryTableViewController {
+    
+    required init!(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+        
+        self.parseClassName = "chat"
+        self.pullToRefreshEnabled = true
+        self.paginationEnabled = false     // load more... button on table view
+        //   self.objectsPerPage = 5
+        
+    }
+    
+    override func queryForTable() -> PFQuery {
+        
+        println("queryForChatTable")
+        
+        var query = PFQuery(className: "chat")
+        query.whereKey("sender1", equalTo: fbUsername)
+        
+        var query2 = PFQuery(className: "chat")
+        query2.whereKey("sender2", equalTo: fbUsername)
+        
+        var combinedQuery = PFQuery.orQueryWithSubqueries([query, query2])
+        
+        return combinedQuery
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let indexPath = tableView.indexPathForSelectedRow();
+        
+        println(tableView.cellForRowAtIndexPath(indexPath!)?.textLabel?.text)
+        
+        selectedChat = tableView.cellForRowAtIndexPath(indexPath!)!.textLabel!.text!
+        
+        println("Selected chat: \(selectedChat)")
+        
+        performSegueWithIdentifier("toChat", sender: self)
+        
+        tableView.deselectRowAtIndexPath(indexPath!, animated: true)
+        
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell? {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell") as? PFTableViewCell
+        
+        println(object)
+        
+        if cell == nil {
+            cell = PFTableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        }
+        if object?["sender1"] as? String == fbUsername {
+            cell?.textLabel?.text = object?["sender2"] as? String
+        } else {
+            cell?.textLabel?.text = object?["sender1"] as? String
+        }
+        
+        cell?.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+        
+        return cell
+    }
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return false
+    }
+    
+    // MARK: - Activity Indicator
+    
+    func addSpinner(Error: String, Animated: Bool) {
+        SwiftSpinner.show(Error, animated: Animated)
+    }
+    
+    func hideSpinner() {
+        SwiftSpinner.hide()
+    }
+    
+    func delay(#seconds: Double, completion:()->()) {
+        let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64( Double(NSEC_PER_SEC) * seconds ))
+        
+        dispatch_after(popTime, dispatch_get_main_queue()) {
+            completion()
+        }
+    }
+    
+    func beginIgnore() {
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+    }
+    func endIgnore() {
+        UIApplication.sharedApplication().endIgnoringInteractionEvents()
+    }
+
+
+}
